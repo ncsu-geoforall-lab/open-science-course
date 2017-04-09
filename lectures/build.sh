b118e41c98456a5a478bfa3f4a3b57558f9f17d9
Vaclav Petras <wenzeslaus@gmail.com>
2017-04-08 20:30:39 -0400

    HTML title and meta

--- lectures/build.sh
+++ lectures/build.sh
@@ -4,8 +4,13 @@ mkdir -p $BUILD_DIR
 
 ./copy-common-files.py --dst-dir=$BUILD_DIR
 
+
+# TODO: split code for special presentations
 for FILE in `ls *.html|grep -v foot|grep -v head`
 do
     ./build-slides.py --outdir=$BUILD_DIR \
+        --title "How to make your research reproducible" \
+        --meta-description "US-IALE 2017 talk about open science" \
+        --meta-author "Vaclav Petras" \
         --outfile=$FILE $FILE
 done
