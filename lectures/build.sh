837049300e004e2f14e2cee9759cab524d8d2488
Vaclav Petras <wenzeslaus@gmail.com>
2017-09-13 12:04:05 -0400

    fix title (still same title for all)

--- lectures/build.sh
+++ lectures/build.sh
@@ -9,8 +9,5 @@ mkdir -p $BUILD_DIR
 for FILE in `ls *.html|grep -v foot|grep -v head`
 do
     ./build-slides.py --outdir=$BUILD_DIR \
-        --title "How to make your research reproducible" \
-        --meta-description "US-IALE 2017 talk about open science" \
-        --meta-author "Vaclav Petras" \
         --outfile=$FILE $FILE
 done
